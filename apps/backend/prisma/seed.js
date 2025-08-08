62af10a98d8da94a4140e879c80540638a2d714e
Miguel Gargurevich <miguel.gargurevich@gmail.com>
2025-08-07 21:16:26 -0500

    chore: elimina client.ts y centraliza uso de PrismaClient en client.js

--- apps/backend/prisma/seed.js
+++ apps/backend/prisma/seed.js
@@ -1,8 +1,6 @@
-const { PrismaClient } = require('@prisma/client');
+const { prisma } = require('./client');
 
 const bcrypt = require('bcryptjs');
-
-const prisma = new PrismaClient();
 
 async function main() {
   await prisma.event.deleteMany({});
--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { PrismaClient } = require('@prisma/client');
+const { prisma } = require('../prisma/client');
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
